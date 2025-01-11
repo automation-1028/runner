9b6d44d694ed435dfb559bd4d3b8d7a9a8add544
tuankg1028 <lethanhtuan1028@gmail.com>
2025-01-11 21:33:09 +0700

    chore: update code

--- src/main.ts
+++ src/main.ts
@@ -76,6 +76,16 @@ async function generateScripts() {
 
         let scripts = getScripts();
         if (scripts.find((s) => s.keyword === keyword)) {
+          // Remove keyword from keywords.txt
+          const keywords = getKeywords();
+          const newKeywords = keywords.filter((k) => k !== keyword);
+          fs.writeFileSync('keywords.txt', newKeywords.join('\n'));
+
+          console.log(
+            `${chalk.green('[generateScripts]')} Script with ${chalk.magenta(
+              keyword,
+            )} keyword already exists!`,
+          );
           return;
         }
         const videoScript = await generateScript(keyword);
@@ -132,220 +142,223 @@ async function generateScripts() {
 }
 
 async function generateVideos() {
-  while (true) {
-    const scripts = getScripts();
+  const genVideo = async (script: Script) => {
+    try {
+      console.log(
+        `${chalk.green(
+          '[generateVideos]',
+        )} Generating video with ${chalk.magenta(script.keyword)} keyword...`,
+      );
 
-    const genVideo = async (script: Script) => {
-      try {
+      // check if video already exists
+      if (script.isLongGenerated) {
+        return;
+      }
+
+      const tagNum = script.tags.split(',').length;
+      const videoTaskRes = await generateVideo({
+        ...DEFAULT_VIDEO_INFO,
+        ...{
+          video_subject: script.title,
+          video_description: script.description,
+          video_terms: tagNum >= 5 ? script.tags : '',
+          thumbnail: script.thumbnail,
+          paragraph_number: 50,
+          subtitle_enabled: false,
+        },
+      } as VideoRequestPayload);
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      let taskRes: any | null;
+      let isFinished = false;
+      do {
+        taskRes = await retry(() => getTask(videoTaskRes.task_id), 10);
+        const { progress } = taskRes;
+
         console.log(
           `${chalk.green(
             '[generateVideos]',
-          )} Generating video with ${chalk.magenta(script.keyword)} keyword...`,
-        );
-
-        await retry(async () => {
-          // check if video already exists
-          if (script.isLongGenerated) {
-            return;
-          }
-
-          const tagNum = script.tags.split(',').length;
-          const videoTaskRes = await generateVideo({
-            ...DEFAULT_VIDEO_INFO,
-            ...{
-              video_subject: script.title,
-              video_description: script.description,
-              video_terms: tagNum >= 5 ? script.tags : '',
-              thumbnail: script.thumbnail,
-              paragraph_number: 50,
-              subtitle_enabled: false,
-            },
-          } as VideoRequestPayload);
-
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          let taskRes: any | null;
-          let isFinished = false;
-          do {
-            taskRes = await retry(() => getTask(videoTaskRes.task_id), 10);
-            const { progress } = taskRes;
-
-            console.log(
-              `${chalk.green(
-                '[generateVideos]',
-              )} Generating video with ${chalk.magenta(
-                script.keyword,
-              )} keyword progress: ${chalk.yellow(progress)}`,
-            );
-            isFinished = progress === 100;
-            if (!isFinished) {
-              await new Promise((resolve) => setTimeout(resolve, 10_000));
-            }
-          } while (!isFinished);
-
-          // Update videos.json
-          const videos = JSON.parse(
-            fs.readFileSync(path.join(__dirname, '../videos.json'), 'utf-8'),
-          );
-          const newVideos = [
-            ...videos,
-            {
-              ...{
-                title: script.title,
-                description: script.description,
-                thumbnail: script.thumbnail,
-                tags: script.tags,
-              },
-              ...videoTaskRes,
-            },
-          ];
-          fs.writeFileSync('videos.json', JSON.stringify(newVideos, null, 2));
-
-          // Update scripts.json
-          const oldScripts = getScripts();
-          const newScripts = oldScripts.map((s) => {
-            if (s.keyword === script.keyword) {
-              return {
-                ...s,
-                isLongGenerated: true,
-              };
-            }
-            return s;
-          });
-          fs.writeFileSync('scripts.json', JSON.stringify(newScripts, null, 2));
-
-          console.log(
-            `${chalk.green(
-              '[generateVideos]',
-            )} Generated video with ${chalk.magenta(script.keyword)} keyword!`,
-          );
-        });
-      } catch (error) {
-        console.error(
-          `${chalk.green(
-            '[generateVideos]',
-          )} Failed to generate video with ${chalk.magenta(
+          )} Generating video with ${chalk.magenta(
             script.keyword,
-          )} keyword due to error: ${(error as Error).message}`,
+          )} keyword progress: ${chalk.yellow(progress)}`,
         );
-      }
-    };
+        isFinished = progress === 100;
+        if (!isFinished) {
+          await new Promise((resolve) => setTimeout(resolve, 10_000));
+        }
+      } while (!isFinished);
 
-    await Bluebird.Promise.map(scripts, genVideo, {
-      concurrency: 1,
-    });
+      // Update videos.json
+      const videos = JSON.parse(
+        fs.readFileSync(path.join(__dirname, '../videos.json'), 'utf-8'),
+      );
+      const newVideos = [
+        ...videos,
+        {
+          ...{
+            title: script.title,
+            description: script.description,
+            thumbnail: script.thumbnail,
+            tags: script.tags,
+          },
+          ...videoTaskRes,
+        },
+      ];
+      fs.writeFileSync('videos.json', JSON.stringify(newVideos, null, 2));
 
-    console.log(
-      `${chalk.green('[generateVideos]')} All videos have been generated!`,
-    );
-    await sleep(30 * 60 * 1000); // 30 mins
+      // Update scripts.json
+      const oldScripts = getScripts();
+      const newScripts = oldScripts.map((s) => {
+        if (s.keyword === script.keyword) {
+          return {
+            ...s,
+            isLongGenerated: true,
+          };
+        }
+        return s;
+      });
+      fs.writeFileSync('scripts.json', JSON.stringify(newScripts, null, 2));
+
+      console.log(
+        `${chalk.green(
+          '[generateVideos]',
+        )} Generated video with ${chalk.magenta(script.keyword)} keyword!`,
+      );
+    } catch (error) {
+      console.error(
+        `${chalk.green(
+          '[generateVideos]',
+        )} Failed to generate video with ${chalk.magenta(
+          script.keyword,
+        )} keyword due to error: ${(error as Error).message}`,
+      );
+    }
+  };
+
+  while (true) {
+    const scripts = getScripts();
+    const script = scripts.find((script) => !script.isLongGenerated);
+    if (!script) {
+      console.log('No long scripts found');
+      await sleep(60_000 * 30); // 1 mins
+      return;
+    }
+
+    await genVideo(script);
+    await sleep(60_000);
   }
 }
 
 async function generateShortVideos() {
+  const genVideo = async (script: Script) => {
+    try {
+      // check if video already exists
+      if (script.isShortGenerated) {
+        return;
+      }
+      console.log(
+        `${chalk.green(
+          '[generateShortVideos]',
+        )} Generating short video with ${chalk.magenta(
+          script.keyword,
+        )} keyword...`,
+      );
+
+      const tagNum = script.tags.split(',').length;
+      const videoTaskRes = await generateVideo({
+        ...DEFAULT_VIDEO_INFO,
+        ...{
+          video_subject: script.title,
+          video_description: script.description,
+          video_terms: tagNum >= 5 ? script.tags : '',
+          thumbnail: script.thumbnail,
+          video_script: '',
+          video_aspect: '9:16',
+          video_clip_duration: 5,
+          font_size: 50,
+          subtitle_position: 'center',
+        },
+      } as VideoRequestPayload);
+
+      let taskRes;
+      let isFinished = false;
+      do {
+        taskRes = await getTask(videoTaskRes.task_id);
+        const { progress } = taskRes;
+
+        console.log(
+          `${chalk.green(
+            '[generateShortVideos]',
+          )} Generating short video with ${chalk.magenta(
+            script.keyword,
+          )} keyword progress: ${chalk.yellow(progress)}`,
+        );
+        isFinished = progress === 100;
+        if (!isFinished) {
+          await new Promise((resolve) => setTimeout(resolve, 10_000));
+        }
+      } while (!isFinished);
+
+      // Update short-videos.json
+      const videos = JSON.parse(
+        fs.readFileSync(path.join(__dirname, '../short-videos.json'), 'utf-8'),
+      );
+      const newVideos = [
+        ...videos,
+        {
+          ...{
+            title: script.title,
+            description: script.description,
+            thumbnail: script.thumbnail,
+            tags: script.tags,
+          },
+          ...videoTaskRes,
+        },
+      ];
+      fs.writeFileSync('short-videos.json', JSON.stringify(newVideos, null, 2));
+
+      // Update scripts.json
+      const oldScripts = getScripts();
+      const newScripts = oldScripts.map((s) => {
+        if (s.keyword === script.keyword) {
+          return {
+            ...s,
+            isShortGenerated: true,
+          };
+        }
+        return s;
+      });
+      fs.writeFileSync('scripts.json', JSON.stringify(newScripts, null, 2));
+
+      console.log(
+        `${chalk.green(
+          '[generateShortVideos]',
+        )} Generated short video with ${chalk.magenta(
+          script.keyword,
+        )} keyword!`,
+      );
+    } catch (error) {
+      console.error(
+        `${chalk.green(
+          '[generateShortVideos]',
+        )} Failed to generate short video with ${chalk.magenta(
+          script.keyword,
+        )} keyword due to error: ${(error as Error).message}`,
+      );
+    }
+  };
+
   while (true) {
     const scripts = getScripts();
+    const script = scripts.find((script) => !script.isShortGenerated);
+    if (!script) {
+      console.log('No short scripts found');
+      await sleep(60_000 * 30); // 1 mins
+      return;
+    }
 
-    await Bluebird.Promise.map(
-      scripts,
-      async (script: Script) => {
-        await retry(async () => {
-          // check if video already exists
-          if (script.isShortGenerated) {
-            return;
-          }
-
-          const tagNum = script.tags.split(',').length;
-          const videoTaskRes = await generateVideo({
-            ...DEFAULT_VIDEO_INFO,
-            ...{
-              video_subject: script.title,
-              video_description: script.description,
-              video_terms: tagNum >= 5 ? script.tags : '',
-              thumbnail: script.thumbnail,
-              video_script: '',
-              video_aspect: '9:16',
-              video_clip_duration: 5,
-              font_size: 50,
-              subtitle_position: 'center',
-            },
-          } as VideoRequestPayload);
-
-          let taskRes;
-          let isFinished = false;
-          do {
-            taskRes = await getTask(videoTaskRes.task_id);
-            const { progress } = taskRes;
-
-            console.log(
-              `${chalk.green(
-                '[generateShortVideos]',
-              )} Generating short video with ${chalk.magenta(
-                script.keyword,
-              )} keyword progress: ${chalk.yellow(progress)}`,
-            );
-            isFinished = progress === 100;
-            if (!isFinished) {
-              await new Promise((resolve) => setTimeout(resolve, 10_000));
-            }
-          } while (!isFinished);
-
-          // Update short-videos.json
-          const videos = JSON.parse(
-            fs.readFileSync(
-              path.join(__dirname, '../short-videos.json'),
-              'utf-8',
-            ),
-          );
-          const newVideos = [
-            ...videos,
-            {
-              ...{
-                title: script.title,
-                description: script.description,
-                thumbnail: script.thumbnail,
-                tags: script.tags,
-              },
-              ...videoTaskRes,
-            },
-          ];
-          fs.writeFileSync(
-            'short-videos.json',
-            JSON.stringify(newVideos, null, 2),
-          );
-
-          // Update scripts.json
-          const oldScripts = getScripts();
-          const newScripts = oldScripts.map((s) => {
-            if (s.keyword === script.keyword) {
-              return {
-                ...s,
-                isShortGenerated: true,
-              };
-            }
-            return s;
-          });
-          fs.writeFileSync('scripts.json', JSON.stringify(newScripts, null, 2));
-
-          console.log(
-            `${chalk.green(
-              '[generateShortVideos]',
-            )} Generated short video with ${chalk.magenta(
-              script.keyword,
-            )} keyword!`,
-          );
-        });
-      },
-      {
-        concurrency: 1,
-      },
-    );
-
-    console.log(
-      `${chalk.green(
-        '[generateShortVideos]',
-      )} All short videos have been generated!`,
-    );
-    await sleep(30 * 60 * 1000); // 30 mins
+    await genVideo(script);
+    await sleep(60_000);
   }
 }
 
@@ -434,6 +447,6 @@ async function autoUpload() {
       await sleep(60_000 * 60); // 60 mins
     }
 
-    await sleep(30_000);
+    await sleep(30); // 1 mins
   }
 }
