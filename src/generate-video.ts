902eec1aa3574470cf79ebdcb438af1094570182
tuankg1028 <lethanhtuan1028@gmail.com>
2025-02-18 23:23:46 +0700

    chore: update

--- src/generate-video.ts
+++ src/generate-video.ts
@@ -208,7 +208,7 @@ async function generateVideos(videoType: VideoType) {
         async (keyword) => {
           await genVideo(keyword);
         },
-        { concurrency: 2 },
+        { concurrency: 3 },
       );
     }
   };
@@ -217,7 +217,7 @@ async function generateVideos(videoType: VideoType) {
   for (const channel of channels) {
     genVideoByChannel(channel);
 
-    // await sleep(60_000 * 10);
+    await sleep(60_000 * 10);
   }
 }
 
